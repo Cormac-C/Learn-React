42db2c36cf9ac7edab0cfe08a2bedc1dbbcf74cf
Cormac Cureton <cormaccureton@gmail.com>
2020-03-11 00:04:49 -0400

    Board controls all squares

--- index.js
+++ index.js
@@ -16,8 +16,26 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
-    renderSquare(i) {
-      return <Square value = {i}/>;
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
+    renderSquare(i){
+      return (
+        <Square 
+          value = {this.state.squares[i]} 
+          onClick = {() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
@@ -47,21 +65,6 @@ class Square extends React.Component {
   }
   
   class Game extends React.Component {
-    constructor(props){
-      super(props);
-      this.state = {
-        squares: Array(9).fill(null),
-      };
-    }
-    
-    renderSquare(i){
-      return 
-        <Square 
-          value={this.state.squares[i]} 
-          onClick={() => this.handleClick(i)}
-        />;
-    }
-
     render() {
       return (
         <div className="game">
